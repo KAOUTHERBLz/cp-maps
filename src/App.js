f23e15da163cc3cfbe4f2969482290e36ea27950
KAWTHERBLz <blouzakawther1@gmail.com>
2023-03-06 10:10:25 +0100

    maps

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Adrees from './components/Adrees';
+
+function App() {
+  return (
+    <div className="App">
+<h1>gomycode Adress</h1>
+<Adrees/>
+    </div>
+  );
+}
+
+export default App;
